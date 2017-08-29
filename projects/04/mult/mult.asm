d650bff1e63a9be74012aeea387575c8da1639c3
Gezoo <gzero_gao@outlook.com>
2017-08-29 11:49:33 +0800

    mult implemention

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@0  
+D=M  //RAM[0] to D register 
+@2  
+M=0 //RAM[2] = 0
+(LOOP)  
+@1  
+D=M  //RAM[1] to D register
+@END  
+D;JEQ  // RAM[1] > 0
+  
+@1  
+M=M-1  // RAM[1]--
+  
+@0  
+D=M  
+@2  
+M=M+D //RAM[2] = RAM[2] + RAM[0]  
+  
+@1  
+D=M  // RAM[1] to D register
+@LOOP  
+D;JGT  
+  
+@END  
+(END)  
+0;JMP 
