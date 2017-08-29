7c4ef10cf87fdbae5125269d04c29d2554b221a1
Gezoo <gzero_gao@outlook.com>
2017-08-29 15:27:06 +0800

    Fill implemention

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(BEGIN)  
+  
+@KBD  
+D = M  
+@CLEAN  
+D;JEQ  
+  
+@SCREEN          //涂黑   
+D = A  
+@Bcount  
+A = M + D  
+M = -1  
+  
+@Bcount          //键盘按下  
+M = M + 1  
+D = M  
+@8192            //8K  
+D = D - A  
+@Bcount  
+M = D  
+@END  
+D;JGE  
+@8192  
+D = D + A  
+@Bcount  
+M = D  
+@END  
+0;JMP  
+  
+(CLEAN)          //无键盘按下   
+  
+@SCREEN          //涂白   
+D = A  
+@Wcount  
+A = M + D  
+M = 0  
+  
+@Wcount          //键盘按下  
+M = M + 1  
+D = M  
+@8192            //8K  
+D = D - A  
+@Wcount  
+M = D  
+@END  
+D;JGE  
+@8192  
+D = D + A  
+@Wcount  
+M = D  
+  
+(END)  
+@BEGIN  
+0;JMP  
